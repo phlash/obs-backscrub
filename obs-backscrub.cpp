7ebfb0cac20972937151525595d5197b04b69812
Phil Ashby <phil.github@ashbysoft.com>
2021-04-27 11:22:55 +0100

    Rename to obs-backscrub

--- obs-backscrub.cpp
+++ obs-backscrub.cpp
@@ -1,4 +1,4 @@
-// Playing with OBS plugins..
+// Simple OBS Studio plugin to use libbackscrub as a background removal filter
 //
 //
 #include <thread>
@@ -7,7 +7,7 @@
 #include <obs-module.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include "libdeepseg.h"
+#include "libbackscrub.h"
 
 // Demo setting name & default value
 static const char DEMO_SETTING[] = "DemoSetting";
@@ -17,7 +17,7 @@ static const int DEMO_DEFAULT = 1;
 void op_printf(const char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
-    printf("obs-play: ");
+    printf("obs-backscrub: ");
     vprintf(fmt, ap);
     fflush(stdout);
     va_end(ap);
@@ -25,7 +25,7 @@ void op_printf(const char *fmt, ...) {
 OBS_DECLARE_MODULE()
 
 // A source, used as a filter..
-struct obs_play_filter_t {
+struct obs_backscrub_filter_t {
     // internal filter state
     calcinfo_t info;
     cv::Mat input;
@@ -38,7 +38,7 @@ struct obs_play_filter_t {
     // filter settings
     int setting;
 };
-static void obs_play_mask_thread(obs_play_filter_t *filter) {
+static void obs_backscrub_mask_thread(obs_backscrub_filter_t *filter) {
     op_printf("mask_thread: starting..\n");
     while (!filter->done) {
         // wait for a fresh video frame
@@ -59,14 +59,14 @@ static void obs_play_mask_thread(obs_play_filter_t *filter) {
     }
     op_printf("mask_thread: done\n");
 }
-static int _obs_play_get_setting(obs_data_t *settings) { return (int)obs_data_get_int(settings, DEMO_SETTING); }
-static const char *obs_play_get_name(void *type_data) { return "Phlash playing about"; }
-static void *obs_play_create(obs_data_t *settings, obs_source_t *source) {
+static int _obs_backscrub_get_setting(obs_data_t *settings) { return (int)obs_data_get_int(settings, DEMO_SETTING); }
+static const char *obs_backscrub_get_name(void *type_data) { return "Background scrubber"; }
+static void *obs_backscrub_create(obs_data_t *settings, obs_source_t *source) {
     op_printf("create\n");
     // here we instantiate a new filter, loading all required resources (eg: model file)
     // and setting initial values for filter settings
-    auto *filter = new obs_play_filter_t;
-    filter->setting = _obs_play_get_setting(settings);
+    auto *filter = new obs_backscrub_filter_t;
+    filter->setting = _obs_backscrub_get_setting(settings);
     filter->info.modelname = "/home/phlash/code/backscrub/models/selfiesegmentation_mlkit-256x256-2021_01_19-v1215.f16.tflite";
     filter->info.threads = 2;
     filter->info.width = 640;
@@ -81,28 +81,28 @@ static void *obs_play_create(obs_data_t *settings, obs_source_t *source) {
     }
     filter->new_frame = false;
     filter->done = false;
-    filter->tid = std::thread(obs_play_mask_thread, filter);
+    filter->tid = std::thread(obs_backscrub_mask_thread, filter);
     return filter;
 }
-static obs_properties_t *obs_play_get_properties(void *state) {
+static obs_properties_t *obs_backscrub_get_properties(void *state) {
     op_printf("get_properties\n");
     obs_properties_t *props = obs_properties_create();
     obs_properties_add_int_slider(props, DEMO_SETTING, "Demonstration setting", -127, 127, 1);
     return props;
 }
-static void obs_play_get_defaults(obs_data_t *settings) {
+static void obs_backscrub_get_defaults(obs_data_t *settings) {
     op_printf("get_defaults\n");
     obs_data_set_default_int(settings, DEMO_SETTING, DEMO_DEFAULT);
 }
-static void obs_play_update(void *state, obs_data_t *settings) {
-    obs_play_filter_t *filter = (obs_play_filter_t *)state;
-    int val = _obs_play_get_setting(settings);
+static void obs_backscrub_update(void *state, obs_data_t *settings) {
+    obs_backscrub_filter_t *filter = (obs_backscrub_filter_t *)state;
+    int val = _obs_backscrub_get_setting(settings);
     op_printf("update: settings=%d->%d\n", filter->setting, val);
     // here we change any filter settings (eg: model used, feathering edges, bilateral smoothing)
     filter->setting = val;
 }
-static void obs_play_destroy(void *state) {
-    obs_play_filter_t *filter = (obs_play_filter_t *)state;
+static void obs_backscrub_destroy(void *state) {
+    obs_backscrub_filter_t *filter = (obs_backscrub_filter_t *)state;
     // stop mask thread
     filter->done = true;
     filter->new_frame = true;
@@ -111,12 +111,12 @@ static void obs_play_destroy(void *state) {
     // free memory
     delete filter;
 }
-static void obs_play_video_tick(void *state, float secs) { }
-static obs_source_frame *obs_play_filter_video(void *state, obs_source_frame *frame) {
-    obs_play_filter_t *filter = (obs_play_filter_t *)state;
+static void obs_backscrub_video_tick(void *state, float secs) { }
+static obs_source_frame *obs_backscrub_filter_video(void *state, obs_source_frame *frame) {
+    obs_backscrub_filter_t *filter = (obs_backscrub_filter_t *)state;
     // here we do the video frame processing
     // First, map data into an OpenCV Mat object, then convert to BGR24 (default OCV format)
-    // for calling libdeepseg
+    // for calling libbackscrub
     cv::Mat out;
     switch (frame->format) {
     // TODO: more video formats!
@@ -157,24 +157,24 @@ static obs_source_frame *obs_play_filter_video(void *state, obs_source_frame *fr
     }
     return frame;
 }
-static struct obs_source_info play_src {
+static struct obs_source_info backscrub_src {
     // required
-    .id = "obs-play",
-    .type = OBS_SOURCE_TYPE_FILTER,             // this displays us in source filter dialog
-    .output_flags = OBS_SOURCE_ASYNC_VIDEO,     // this means we only need filter_video()
-    .get_name = obs_play_get_name,              // this name is shown in source filter dialog
-    .create = obs_play_create,                  // new instance of this filter
-    .destroy = obs_play_destroy,                // delete filter instance
+    .id = "obs-backscrub",
+    .type = OBS_SOURCE_TYPE_FILTER,                  // this displays us in source filter dialog
+    .output_flags = OBS_SOURCE_ASYNC_VIDEO,          // this means we only need filter_video()
+    .get_name = obs_backscrub_get_name,              // this name is shown in source filter dialog
+    .create = obs_backscrub_create,                  // new instance of this filter
+    .destroy = obs_backscrub_destroy,                // delete filter instance
     // optional
-    .get_defaults = obs_play_get_defaults,      // gets default values of properties (settings)
-    .get_properties = obs_play_get_properties,  // defines user-adjustable properties (settings)
-    .update = obs_play_update,                  // change the filter settings
-    .video_tick = obs_play_video_tick,          // frame duration supplied (not absolute time)
-    .filter_video = obs_play_filter_video       // process one input to one output frame
+    .get_defaults = obs_backscrub_get_defaults,      // gets default values of properties (settings)
+    .get_properties = obs_backscrub_get_properties,  // defines user-adjustable properties (settings)
+    .update = obs_backscrub_update,                  // change the filter settings
+    .video_tick = obs_backscrub_video_tick,          // frame duration supplied (not absolute time)
+    .filter_video = obs_backscrub_filter_video       // process one input to one output frame
 };
 bool obs_module_load(void) {
     op_printf("load\n");
-    // here we take the opportunity to ensure dependent components (eg: libdeepseg) are loadable
-    obs_register_source(&play_src);
+    // here we take the opportunity to ensure dependent components (eg: libbackscrub) are loadable
+    obs_register_source(&backscrub_src);
     return true;
 }
